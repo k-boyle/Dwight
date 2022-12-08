0db40e64ac1f8b804a2826ad6675cbbf009107d3
Kieran <kayrenhi@gmail.com>
2022-12-08 22:44:18 +0000

    Added names of alts to mentions

--- Services/WarReminderService.cs
+++ Services/WarReminderService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Text.RegularExpressions;
@@ -172,65 +173,87 @@ public class WarReminderService : DiscordBotService
                     else
                     {
                         var endsIn = currentWar.EndTime - DateTimeOffset.UtcNow;
-                        switch (currentWar.Cwl)
+                        if (CwlShouldRemind(currentWar, endsIn, currentReminder))
                         {
-                            case true when endsIn < TimeSpan.FromHours(4) && currentReminder.CwlRemindersPosted < 4 && (currentReminder.CwlRemindersPosted == 0 || currentReminder.CwlReminderLastPosted != endsIn.Hours):
+                            Logger.LogInformation("Posting cwl attack reminders for {ClanTag}", clanTag);
+
+                            var missedAttackers = currentWar.Clan.Members.Where(member => member.Attacks == null);
+
+                            var missedAttacksByOwner = new Dictionary<ulong, (ClashMember, List<WarMember>)>();
+                            foreach (var missedAttacker in missedAttackers)
                             {
-                                Logger.LogInformation("Posting cwl attack reminders for {ClanTag}", clanTag);
-
-                                var missedAttacks = currentWar.Clan.Members.Where(member => member.Attacks == null)
-                                    .Select(member => member.Tag)
-                                    .ToHashSet();
-
-                                if (missedAttacks.Count == 0)
+                                var owner = settings.Members.FirstOrDefault(member => member.Tags.Any(tag => tag == missedAttacker.Tag));
+                                if (owner == null)
                                     continue;
 
-                                var inDiscord = settings.Members.Where(member => member.Tags.Any(tag => missedAttacks.Contains(tag)));
-                                var mentions = string.Join(", ", inDiscord.Select(member => Mention.User(member.DiscordId)));
-
-                                var message = new LocalMessage
+                                if (missedAttacksByOwner.TryGetValue(owner.DiscordId, out var thingy))
                                 {
-                                    Content = $"War ends in {endsIn.Hours + 1} hour(s),\n{mentions}\n\nYou still need to attack!"
-                                };
-                                await warChannel.SendMessageAsync(message, cancellationToken: cancellationToken);
-
-                                currentReminder.CwlRemindersPosted++;
-                                currentReminder.CwlReminderLastPosted = endsIn.Hours;
-                                save = true;
-                                break;
-                            }
-                            case false when !currentReminder.ReminderPosted && endsIn < TimeSpan.FromHours(1):
-                            {
-                                Logger.LogInformation("Posting attack reminders for {ClanTag}", clanTag);
-
-                                var missedAttacks = currentWar.Clan.Members.Where(member => member.Attacks is null or { Length: < 2 })
-                                    .Where(member => Remind(settings, member.Tag))
-                                    .Select(member => member.Tag)
-                                    .ToHashSet();
-
-                                if (missedAttacks.Count == 0)
-                                {
-                                    Logger.LogInformation("No missed attacks for {ClanTag}", clanTag);
-                                    continue;
+                                    thingy.Item2.Add(missedAttacker);
                                 }
-
-                                var inDiscord = settings.Members.Where(member => member.Tags.Any(tag => missedAttacks.Contains(tag)));
-                                var mentions = string.Join(", ", inDiscord.Select(member => Mention.User(member.DiscordId)));
-
-                                var message = new LocalMessage
+                                else
                                 {
-                                    Content = $"War ends soon! \n{mentions}\n\nYou still need to attack!"
-                                };
-                                await warChannel.SendMessageAsync(message, cancellationToken: cancellationToken);
-
-                                currentReminder.ReminderPosted = true;
-                                save = true;
-                                break;
+                                    missedAttacksByOwner[owner.DiscordId] = (owner, new() { missedAttacker });
+                                }
                             }
 
-                            default:
-                                Logger.LogInformation("Nothing to post for {ClanTag}", clanTag);
-                                break;
+                            if (missedAttacksByOwner.Count == 0)
+                                continue;
+
+                            var missedAttackMentions = missedAttacksByOwner.Select(missedAttacker => Mention.User(missedAttacker.Key) + GetMembersSuffix(missedAttacker.Value.Item1, missedAttacker.Value.Item2));
+                            var mentions = string.Join(", ", missedAttackMentions);
+
+                            var message = new LocalMessage
+                            {
+                                Content = $"War ends in {endsIn.Hours + 1} hour(s),\n{mentions}\n\nYou still need to attack!"
+                            };
+                            await warChannel.SendMessageAsync(message, cancellationToken: cancellationToken);
+
+                            currentReminder.CwlRemindersPosted++;
+                            currentReminder.CwlReminderLastPosted = endsIn.Hours;
+                            save = true;
+                        }
+                        else if (!currentWar.Cwl && !currentReminder.ReminderPosted && endsIn < TimeSpan.FromHours(1))
+                        {
+                            Logger.LogInformation("Posting attack reminders for {ClanTag}", clanTag);
+
+                            var missedAttackers = currentWar.Clan.Members.Where(member => member.Attacks is null or { Length: < 2 })
+                                .Where(member => Remind(settings, member.Tag));
+
+                            var missedAttacksByOwner = new Dictionary<ulong, (ClashMember, List<WarMember>)>();
+                            foreach (var missedAttacker in missedAttackers)
+                            {
+                                var owner = settings.Members.FirstOrDefault(member => member.Tags.Any(tag => tag == missedAttacker.Tag));
+                                if (owner == null)
+                                    continue;
+
+                                if (missedAttacksByOwner.TryGetValue(owner.DiscordId, out var thingy))
+                                {
+                                    thingy.Item2.Add(missedAttacker);
+                                }
+                                else
+                                {
+                                    missedAttacksByOwner[owner.DiscordId] = (owner, new() { missedAttacker });
+                                }
+                            }
+
+                            if (missedAttacksByOwner.Count == 0)
+                                continue;
+
+                            var missedAttackMentions = missedAttacksByOwner.Select(missedAttacker => Mention.User(missedAttacker.Key) + GetMembersSuffix(missedAttacker.Value.Item1, missedAttacker.Value.Item2));
+                            var mentions = string.Join(", ", missedAttackMentions);
+
+                            var message = new LocalMessage
+                            {
+                                Content = $"War ends soon! \n{mentions}\n\nYou still need to attack!"
+                            };
+                            await warChannel.SendMessageAsync(message, cancellationToken: cancellationToken);
+
+                            currentReminder.ReminderPosted = true;
+                            save = true;
+                        }
+                        else
+                        {
+                            Logger.LogInformation("Nothing to post for {ClanTag}", clanTag);
                         }
                     }
 
@@ -318,5 +341,23 @@ public class WarReminderService : DiscordBotService
     private static bool Remind(GuildSettings settings, string tag)
     {
         return settings.Members.FirstOrDefault(member => Array.FindIndex(member.Tags, str => str == tag) != -1)?.Remind ?? false;
+    }
+
+    private static bool CwlShouldRemind(CurrentWarData currentWar, TimeSpan endsIn, CurrentWarReminder currentReminder)
+    {
+        return currentWar.Cwl 
+            && endsIn < TimeSpan.FromHours(4) 
+            && currentReminder.CwlRemindersPosted < 4 
+            && (currentReminder.CwlRemindersPosted == 0 || currentReminder.CwlReminderLastPosted != endsIn.Hours);
+    }
+    
+    private static string GetMembersSuffix(ClashMember member, List<WarMember> warMembers)
+    {
+        if (warMembers.Count > 1)
+            return $" ({string.Join(", ", warMembers.Select(warMember => warMember.Name))})";
+
+        var warMember = warMembers[0];
+        var index = Array.IndexOf(member.Tags, warMember.Tag);
+        return index != member.MainTag ? $" ({warMember.Name})" : "";
     }
 }
