1683b43761e2fd2c2a116d8ff99c2a635335667b
Kieran <kayrenhi@gmail.com>
2025-06-17 11:09:22 +0100

    people still try !

---
+++ Services/TheYearIs2025Service.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Disqord;
+using Disqord.Bot.Hosting;
+using Disqord.Rest;
+
+namespace Dwight;
+
+public class TheYearIs2025Service : DiscordBotService
+{
+    protected override async ValueTask OnMessageReceived(BotMessageReceivedEventArgs e)
+    {
+        if (e.Message.Content[0] != '!') return;
+
+        await Bot.SendMessageAsync(e.ChannelId,
+            new()
+            {
+                Content = "It's 2025, it's / commands",
+                Reference = new LocalMessageReference { MessageId = e.MessageId }
+            });
+    }
+}
