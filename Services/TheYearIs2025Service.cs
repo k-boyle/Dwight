022b0fa10d8d124ae087ade99dea0dcf865dadc7
Kieran <kayrenhi@gmail.com>
2025-06-17 15:07:35 +0100

    bug bug bug

--- Services/TheYearIs2025Service.cs
+++ Services/TheYearIs2025Service.cs
@@ -9,7 +9,7 @@ public class TheYearIs2025Service : DiscordBotService
 {
     protected override async ValueTask OnMessageReceived(BotMessageReceivedEventArgs e)
     {
-        if (e.Message.Content.Length == 0 || e.Message.Content[0] != '!') return;
+        if (e.Message.Author.IsBot || e.Message.Content.Length == 0 || e.Message.Content[0] != '!') return;
 
         await Bot.SendMessageAsync(e.ChannelId,
             new()
