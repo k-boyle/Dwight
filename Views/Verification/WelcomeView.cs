dab861441275dc97686298e747dc72d247b02f31
Kieran <kayrenhi@gmail.com>
2025-07-21 20:50:41 +0100

    inf timeout on welcome view

--- Views/Verification/WelcomeView.cs
+++ Views/Verification/WelcomeView.cs
@@ -57,79 +57,72 @@ public class WelcomeView(string guildName, Dictionary<string, string> baseLinkBy
         var waitForResponse = Menu.Client.GetInteractivity()
             .WaitForInteractionAsync<IModalSubmitInteraction>(
                 e.ChannelId, modal => modal.CustomId == modalId && modal.AuthorId == e.AuthorId,
-                TimeSpan.FromMinutes(20)
+                Timeout.InfiniteTimeSpan
             );
 
         await e.Interaction.Response().SendModalAsync(modalResponse);
 
         var modal = await waitForResponse;
 
-        if (modal != null)
-        {
-            var components = modal.Components;
-            var row1Response = components[0] as IRowComponent;
-            var row2Response = components[1] as IRowComponent;
-            var row3Response = components[2] as IRowComponent;
+        if (modal == null)
+            return;
 
-            var tagResponse = row1Response!.Components[0] as ITextInputComponent;
-            var keyResponse = row2Response!.Components[0] as ITextInputComponent;
-            var passwordResponse = row3Response!.Components[0] as ITextInputComponent;
+        var components = modal.Components;
+        var row1Response = components[0] as IRowComponent;
+        var row2Response = components[1] as IRowComponent;
+        var row3Response = components[2] as IRowComponent;
 
-            var bot = e.Interaction.Client as DiscordBot;
-            var apiClient = bot!.Services.GetRequiredService<ClashApiClient>();
-            
-            var verifiedToken = await apiClient.VerifyTokenAsync(tagResponse!.Value!, new VerifyToken(keyResponse!.Value!), CancellationToken.None);
-            if (verifiedToken?.Status != "ok")
-            {
-                var response = new LocalInteractionMessageResponse()
-                    .WithContent("Failed to verify, token expired (they only last a couple minutes) or wrong player tag specified")
-                    .WithIsEphemeral();
-                await modal.Response().SendMessageAsync(response);
-                return;
-            }
+        var tagResponse = row1Response!.Components[0] as ITextInputComponent;
+        var keyResponse = row2Response!.Components[0] as ITextInputComponent;
+        var passwordResponse = row3Response!.Components[0] as ITextInputComponent;
 
-            if (!passwordResponse!.Value!.Equals(password, StringComparison.InvariantCultureIgnoreCase))
-            {
-                var response = new LocalInteractionMessageResponse()
-                    .WithContent($"Incorrect RCS password, it can be found {Markdown.Link("here", "https://www.reddit.com/r/RedditClanSystem/wiki/official_reddit_clan_system/")}")
-                    .WithIsEphemeral();
-                await modal.Response().SendMessageAsync(response);
-                return;
-            }
+        var bot = e.Interaction.Client as DiscordBot;
+        var apiClient = bot!.Services.GetRequiredService<ClashApiClient>();
 
-            var completeResponse = new LocalInteractionMessageResponse()
-                .WithContent("Thank you for doing the self verification, wait for a co-eld to accept you")
-                .WithIsEphemeral();
-            await modal.Response().SendMessageAsync(completeResponse);
-
-            ClearComponents();
-
-            var player = await apiClient.GetPlayerAsync(verifiedToken!.Tag, CancellationToken.None);
-            var tag = verifiedToken.Tag;
-
-            var tagString = Markdown.Escape($"{player?.Name}{tag}");
-            var verifiedEmbed = new LocalEmbed()
-                .WithTitle("Self Verification Complete")
-                .WithColor(new(0x11f711))
-                .WithDescription($"{modal.Author.Mention} has completed self verification, they own the account {Markdown.Code(tagString)}");
-
-            await bot.SendMessageAsync(e.ChannelId, new() { Content = $"https://cc.fwafarm.com/cc_n/member.php?tag={tag[1..]}" });
-
-            var verificationCompletedView = new VerificationCompletedView(
-                message => message.WithEmbeds(verifiedEmbed),
-                e.AuthorId,
-                tag
-            );
-            var menu = new DefaultTextMenu(verificationCompletedView);
-            await bot.StartMenuAsync(e.ChannelId, menu);
-        }
-        else
+        var verifiedToken = await apiClient.VerifyTokenAsync(tagResponse!.Value!, new VerifyToken(keyResponse!.Value!), CancellationToken.None);
+        if (verifiedToken?.Status != "ok")
         {
             var response = new LocalInteractionMessageResponse()
-                .WithContent("You did not respond...")
+                .WithContent("Failed to verify, token expired (they only last a couple minutes) or wrong player tag specified")
                 .WithIsEphemeral();
-            await e.Interaction.Response().SendMessageAsync(response);
+            await modal.Response().SendMessageAsync(response);
+            return;
         }
+
+        if (!passwordResponse!.Value!.Equals(password, StringComparison.InvariantCultureIgnoreCase))
+        {
+            var response = new LocalInteractionMessageResponse()
+                .WithContent($"Incorrect RCS password, it can be found {Markdown.Link("here", "https://www.reddit.com/r/RedditClanSystem/wiki/official_reddit_clan_system/")}")
+                .WithIsEphemeral();
+            await modal.Response().SendMessageAsync(response);
+            return;
+        }
+
+        var completeResponse = new LocalInteractionMessageResponse()
+            .WithContent("Thank you for doing the self verification, wait for a co-eld to accept you")
+            .WithIsEphemeral();
+        await modal.Response().SendMessageAsync(completeResponse);
+
+        ClearComponents();
+
+        var player = await apiClient.GetPlayerAsync(verifiedToken!.Tag, CancellationToken.None);
+        var tag = verifiedToken.Tag;
+
+        var tagString = Markdown.Escape($"{player?.Name}{tag}");
+        var verifiedEmbed = new LocalEmbed()
+            .WithTitle("Self Verification Complete")
+            .WithColor(new(0x11f711))
+            .WithDescription($"{modal.Author.Mention} has completed self verification, they own the account {Markdown.Code(tagString)}");
+
+        await bot.SendMessageAsync(e.ChannelId, new() { Content = $"https://cc.fwafarm.com/cc_n/member.php?tag={tag[1..]}" });
+
+        var verificationCompletedView = new VerificationCompletedView(
+            message => message.WithEmbeds(verifiedEmbed),
+            e.AuthorId,
+            tag
+        );
+        var menu = new DefaultTextMenu(verificationCompletedView);
+        await bot.StartMenuAsync(e.ChannelId, menu);
     }
 
     private static LocalEmbed CreateWelcomeEmbed(string guildName, Snowflake userId, Dictionary<string, string> baseLinkByLevel)
